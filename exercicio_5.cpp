937ad4c39429346caa44772c5cb3aaf97bf0a4a8
Joey <josiasdsj1@gmail.com>
2022-07-25 20:35:34 -0300

    Update exercicio_5.cpp

--- exercicio_5.cpp
+++ exercicio_5.cpp
@@ -1,42 +1,62 @@
-#include <C:\Users\003908631\Desktop\CG\glut\glut.h>
+#include <GL/glut.h>
 
-//Exercio numero 5 - Viewport
+// Questão numero 5 - Viewport
 
-
-float R = 1.0; 
-float G = 1.0; 
+float R = 0.0;
+float G = 0.0;
 float B = 1.0;
 
-void Draw(){
-    glClearColor( 0.0, 0.0, 0.0, 1.0 );
 
+void retangle();
+void changeScreen(GLsizei w, GLsizei h);
+
+void Draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    retangle();
 }
 
-int main(int argc, char** argv){
-    int mode = GLUT_RGB | GLUT_DOUBLE;glutInitDisplayMode( mode );
-    glutCreateWindow( argv[0] );  
-    init( );
-
-    glutDisplayFunc( Draw );
-    //glutReshapeFunc( reshape ); 
-    //glutKeyboardFunc( key );
-    //glutIdleFunc( idle ); 
-    
-    glutMainLoop( );
+void retangle()
+{
+    glClearColor(0.0,0.0,0.0,0.0);
+    glBegin(GL_POLYGON);
+    glColor3f(R, G, B);
+    glVertex2f(2.0, 6.0);
+    glVertex2f(8.0, 6.0);
+    glVertex2f(8.0, 4.0);
+    glVertex2f(2.0, 4.0);
+    glEnd();
+    glFlush();
 }
 
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    int mode = GLUT_RGB | GLUT_SINGLE;
+    glutInitDisplayMode(mode);
+    glutCreateWindow("Questao 5");
+    glutInitWindowSize(500,500);
+    glutDisplayFunc(Draw);
+    glutReshapeFunc(changeScreen);
+    glutMainLoop();
+}
 
-voic changeScreen(GLsizei w, GLsizei h){
-    if(h == 0){
+void changeScreen(GLsizei w, GLsizei h)
+{
+    if (h == 0)
+    {
         h = 1;
     }
-    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    if(w <= h){
-        gluOrtho2D(0.0f,50.0f,0.0f, 50.0f*h/w);
-    }else{
-        
-        gluOrtho2D(0.0f,50.0f*w/h,0.0f,50.0f);
+    if (w <= h)
+    {
+        gluOrtho2D(0.0f, 50.0f, 0.0f, 50.0f * h/w);
+    }
+    else
+    {
+        gluOrtho2D(0.0f, 50.0f * w/h, 0.0f, 50.0f);
     }
 }
